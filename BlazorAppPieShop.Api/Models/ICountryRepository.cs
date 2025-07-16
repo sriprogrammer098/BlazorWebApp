6dd7b3a0fc6585fddfca5e96d4caaf9b3f13850f
Sriprogrammer098 <sriprogrammer6@gmail.com>
2025-07-16 13:47:45 -0500

    Added API references

---
+++ BlazorAppPieShop.Api/Models/ICountryRepository.cs
@@ -0,0 +1,11 @@
+﻿using BlazorAppPieShop.Shared;
+using System.Collections.Generic;
+
+namespace BlazorAppPieShop.Api.Models
+{
+    public interface ICountryRepository
+    {
+        IEnumerable<Country> GetAllCountries();
+        Country GetCountryById(int countryId);
+    }
+}
